f5e2b415ad4507339c15290c20f928f38bbc5193
Fabian <fabian@arctium.io>
2018-04-06 14:05:05 +0200

    Added Arctium.Shared project to store things like database entity classes etc. Added some empty Aurora database entity classes.

---
+++ src/Arctium.Shared/src/Database/Aurora/Entities/BnetVersion.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) Arctium.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Lappa.ORM;
+
+namespace Arctium.Database.Aurora.Entities
+{
+    public class BnetVersion : Entity
+    {
+        
+    }
+}
