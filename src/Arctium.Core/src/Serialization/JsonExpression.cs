efd4036a76085b4dd903e359b8c4a2e55cc88352
Fabian <fabian@arctium.io>
2018-04-08 22:54:12 +0200

    Added JsonExpression class to allow serialization/deserialization of Expression objects.

---
+++ src/Arctium.Core/src/Serialization/JsonExpression.cs
@@ -0,0 +1,37 @@
+﻿// Copyright (c) Arctium.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Aq.ExpressionJsonSerializer;
+using Newtonsoft.Json;
+using System;
+using System.Reflection;
+
+namespace Arctium.Core.Serialization
+{
+    public static class JsonExpression
+    {
+        static JsonSerializerSettings serializerSettings;
+
+        static JsonExpression()
+        {
+            serializerSettings = new JsonSerializerSettings
+            {
+                TypeNameHandling = TypeNameHandling.Objects
+            };
+        }
+
+        public static void AddConverter(Assembly typeAssembly)
+        {
+            serializerSettings.Converters.Add(new ExpressionJsonConverter(typeAssembly));
+        }
+
+        public static void AddConverter(Type type)
+        {
+            serializerSettings.Converters.Add(new ExpressionJsonConverter(Assembly.GetAssembly(type)));
+        }
+
+        public static string Serialize<T>(T obj) => JsonConvert.SerializeObject(obj, serializerSettings);
+
+        public static T Deserialize<T>(string json) => JsonConvert.DeserializeObject<T>(json, serializerSettings);
+    }
+}
